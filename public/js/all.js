e87e2a91ed5e274d5674f0b0587451d5693e4559
admin <admin@testing.default.tdobson.uk0.bigv.io>
2014-08-30 22:21:55 +0100

    fixed silly javascript by morons

--- public/js/all.js
+++ public/js/all.js
@@ -1,8 +1,8 @@
 function addUser(){
             
-    window.location.href = '/customers/add';
+    window.location.href = '/db1/add';
 }
 function cancelAdd(){
     
-    window.location.href = '/customers';
+    window.location.href = '/db1';
 }
