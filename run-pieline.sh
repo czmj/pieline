01eb7615f010a267f3e64510c9dd466e31b7f85d
admin <admin@testing.default.tdobson.uk0.bigv.io>
2015-02-21 21:45:28 +0000

    urgh lots of uncommited changes
    actuallyed added it now

---
+++ run-pieline.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [[ $1 = "live" ]] ; then
+	TYPE=live
+        echo "live mode"
+	screen -s $TYPE bash -c "cd /srv/pieline.net/public/htdocs/nodecrud/;while :; do echo 'Hit CTRL+C to restart'; NODE_ENV=live node app.js;done" ;
+	screen -ls
+else
+	TYPE=dev
+	echo "Development mode"
+	screen -s $TYPE bash -c "cd /srv/pieline.net/public/htdocs/nodecrud/;echo 'Hit CTRL+C to quit'; NODE_ENV=development node app.js" ;
+fi
