5af5422144bb607d737af7e09b14bbad9a1146ea
admin <admin@testing.default.tdobson.uk0.bigv.io>
2015-02-22 02:00:04 +0000

    updated paths

--- run-pieline.sh
+++ run-pieline.sh
@@ -2,10 +2,10 @@
 if [[ $1 = "live" ]] ; then
 	TYPE=live
         echo "live mode"
-	screen -s $TYPE bash -c "cd /srv/pieline.net/public/htdocs/nodecrud/;while :; do echo 'Hit CTRL+C to restart'; NODE_ENV=live node app.js;done" ;
+	screen -s $TYPE bash -c "cd /srv/pieline.net/public/htdocs/pieline/;while :; do echo 'Hit CTRL+C to restart'; NODE_ENV=live node app.js;done" ;
 	screen -ls
 else
 	TYPE=dev
 	echo "Development mode"
-	screen -s $TYPE bash -c "cd /srv/pieline.net/public/htdocs/nodecrud/;echo 'Hit CTRL+C to quit'; NODE_ENV=development node app.js;sleep 10" ;
+	screen -s $TYPE bash -c "cd /srv/pieline.net/public/htdocs/pieline/;echo 'Hit CTRL+C to quit'; NODE_ENV=development node app.js;sleep 10" ;
 fi
