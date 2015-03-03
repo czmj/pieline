65c55ea30342df93198451436a8e4113086bf27c
czmj <clarazmjordan@gmail.com>
2015-03-03 11:17:50 +0000

    active nav class on categories 9and fixed on cities)

--- public/js/all.js
+++ public/js/all.js
@@ -6,3 +6,42 @@ function cancelAdd(){
     
     window.location.href = '/db1';
 }
+
+// dynamically activate list items when clicked
+$( document ).ready(function() {
+        var href=window.location.href.split("/");;
+        var city=href[4];
+        var category=href[5];
+
+        $(".nav").find(".active").removeClass("active");
+
+        //select city
+        if (city==="Manchester"){
+                $("#Manchester").addClass("active");
+        }else if(city==="Sheffield"){
+                $("#Sheffield").addClass("active");
+        }else if(city==="Leeds"){
+                $("#Leeds").addClass("active");
+        }else if(city==="Liverpool"){
+        	$("#Liverpool").addClass("active");
+	}
+
+	//select category
+	if(category==="Development"){
+		$("#dev").addClass("active");
+	}else if(category==="Design"){
+		$("#design").addClass("active");
+	}else if(category==="Support"){
+                $("#tech").addClass("active");
+	}else if(category==="Business"){
+                $("#business").addClass("active");
+	}else if(category==="Communications"){
+                $("#comms").addClass("active");
+	}else{
+		$("#all").addClass("active");
+	}
+
+
+
+});
+
